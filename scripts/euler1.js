4990df023e884c81deb8a412f496d57be7bda730
jonmccull <jonmc@vivaldi.com>
2017-10-18 22:14:32 +0200

    Cleaning up Euler1

--- scripts/euler1.js
+++ scripts/euler1.js
@@ -1,25 +1,18 @@
 
-
-
-window.onload = function() { // Set up event listener for the new limit input.
-    
+window.onload = function() { // Sets up event listener for the new limit input.
     document.getElementById("submitBtn").addEventListener("click", sumOfMultiples(), false);
 }
 
-function getNewLimit() { // Get the new inputed number
-	console.log("getNewLimit triggered");
-
+function getNewLimit() { // Gets the new inputed number and returns it.
 	var newLimit = document.getElementById("maxNumInput").value;
 	return newLimit; 
 }
 
-function updateDisplay(sum) { // Update the sum displayed to user
+function updateDisplay(sum) { // Updates the sum displayed to user
 	document.getElementById("numDisplay").innerHTML = "The new sum is " + sum;
 }
 
-function sumOfMultiples() { // Calc sum of all multiples, taking max number as input. Returns a number.
-	
-	console.log("New sum calc started");
+function sumOfMultiples() { // Calcs sum of all multiples, taking max number as input. Returns a number.
 
 	var limit = getNewLimit();
 	var sum = 0;					
